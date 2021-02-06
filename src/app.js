e4f30e7c4f41185fa63f6effdbf947fd06bc38d0
alineolic <72208027+alineolic@users.noreply.github.com>
2021-02-06 22:40:36 +0000

    Added hourly forecast

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 function formatDate(timestamp) {
     let date = new Date (timestamp);
+    
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let day = days[date.getDay()];
+return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+    let date = new Date (timestamp);
     let hours = date.getHours();
     if (hours < 10) {
     hours = `0${hours}`
-}
+    }
     let minutes = date.getMinutes();
 if (minutes < 10) {
     minutes = `0${minutes}`
 }
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        let day = days[date.getDay()];
-return `${day} ${hours}:${minutes}`;
+    return `${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -38,10 +44,35 @@ function displayTemperature(response) {
     );
 }
 
+function displayForecast(response) {
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = null;
+let forecast = null;
+
+for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+<div class="col-2">
+<h3>
+${formatHours(forecast.dt * 1000)}
+</h3>
+<img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" />
+<div class="weather-forecast-temperature">
+<strong>${Math.round(forecast.main.temp_max)}°</strong>  ${Math.round(forecast.main.temp_min)}°
+</div>
+</div>
+`;
+}
+}
+
+
 function search(city) {
 let apiKey = "3a5b83d2290ca8aeea736a82a10e7ea7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
+
+apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
